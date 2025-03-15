19096424385f71f255d769cff6aca2aff88c3d2c
Romeo <120739238+N00body-romeo@users.noreply.github.com>
2025-03-15 11:42:07 +0100

    Add files via upload

---
+++ Lab02/ex01/Store_item/GenericItem.py
@@ -0,0 +1,4 @@
+class GenericItem:
+    def __init__(self,name,s_hand):
+        self.s_hand = s_hand
+        self.name = name
---
+++ Lab02/ex01/Store_item/NormalItem.py
@@ -0,0 +1,4 @@
+class NormalItem:
+    def __init__(self,name,s_hand):
+        self.s_hand = s_hand
+        self.name = name
